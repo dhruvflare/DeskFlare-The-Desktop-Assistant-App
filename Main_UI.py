74d25fd112e3c5de77ca8f9300f0078e88df9eb8
LAPTOP-1OLM3132\dhruv <dhruvflare@gmail.com>
2023-06-05 17:42:42 +0530

    simple voice query update
    it can now take simple voice query,
    say stuff, change language and exit...

--- Main_UI.py
+++ Main_UI.py
@@ -1,7 +1,44 @@
-# project 1
+# imports
+import pyaudio
+import speech_recognition as sr
+import wikipedia
+import win32com.client
+
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+
+def say(text):
+    speaker.Speak(text)
+
+
+def take_command(lan='en-us'):
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio, language=lan)
+            print(f'user said {query}')
+            return query
+
+        except Exception as e:
+            return "Could not recognise, please say again..."
+
 
 def main():
-    pass
+    say('hello i am jarvis AI')
+    text = ''
+    language = 'en-us'
+    while text.lower() != "exit":
+        if text.lower() == 'speak hindi':
+            language = 'en-in'
+
+        elif text.lower() == 'speak english':
+            language = 'en-us'
+
+        print(f"listening..[{language}]")
+        text = take_command(lan=language)
+        # say(text)
 
 
 if __name__ == '__main__':
