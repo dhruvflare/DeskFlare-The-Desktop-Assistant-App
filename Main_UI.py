28e32a90d13d309784d2f4b5026dd92f8347c0c8
LAPTOP-1OLM3132\dhruv <dhruvflare@gmail.com>
2023-06-05 22:39:58 +0530

    fail

--- Main_UI.py
+++ Main_UI.py
@@ -1,24 +1,62 @@
 # imports
+# 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
 # import pyaudio
 import speech_recognition as sr
 # import wikipedia
 import win32com.client
 import webbrowser
-# import openai
+import openai
 import AppOpener
 import datetime
+import os
 
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
 sites = [
-                ["youtube", "https://www.youtube.com/"],
-                ["google", "https://www.google.com/"],
-                ["mail", "https://mail.google.com/mail/u/0/#inbox"]
-            ]
+    ["youtube", "https://www.youtube.com/"],
+    ["google", "https://www.google.com/"],
+    ["mail", "https://mail.google.com/mail/u/0/#inbox"]
+]
 apps = [
     "spotify",
     "discord"
 ]
+
+
+def ai(prompt):
+
+    openai.api_key = "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"
+    text = f"openai response for Prompt: {prompt} \n ************** \n\n\n\n"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    text += response["choices"][0]["text"]
+    if not os.path.exists("./OpenAi"):
+        os.mkdir("./OpenAI")
+
+    if not os.path.exists("./OpenAi/ConfigText.txt"):
+        f = open("./OpenAi/ConfigText.txt", "w")
+        f.write("1")
+        f.close()
+
+    with open("./OpenAi/ConfigText.txt", "a+") as f:
+        f.seek(0, 0)
+        with open(f"./OpenAI/Prompt - {int(f.read()[-1])}", "w") as fh:
+            fh.write(text)
+
+        x = str(int(f.read()[-1]) + 1)
+        f.seek(0, 2)
+        f.write(x)
+
+    return response["choices"][0]["text"]
 
 
 def say(text):
@@ -71,6 +109,10 @@ def main():
                 if f"open {app}" in text.lower():
                     say(f"opening {app} sir")
                     AppOpener.open(app)
+
+        elif "using artificial intelligence" in text.lower():
+            ret = ai(prompt=text)
+            say(ret)
 
         elif "the time" in text.lower():
             timeval = datetime.datetime.now().strftime("%H:%M")
